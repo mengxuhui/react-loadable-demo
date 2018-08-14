b0c69b78f97175af9f7ddaa741f919661c0fd524
mengxuhui214175 <mengxuhui214175@sogou-inc.com>
2018-08-14 14:14:27 +0800

    update files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './components/require_ensure/app';
 
 ReactDOM.render(<App/>,document.getElementById('root'));
